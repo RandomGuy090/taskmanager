71fd907384b37087cdd3093e6355808b01052aa8
RandomGuy090 <randomguy0090@gmail.com>
2021-09-27 22:13:18 +0200

    permissions added

---
+++ api/permissions.py
@@ -0,0 +1,87 @@
+from rest_framework import permissions
+from taskmanager.exceptions import (
+	ServiceUnavailable,
+	Unauthorized,
+	NotAdded,
+	NoSuchTable,
+)
+from tables.models import (
+	Notes, 
+	Tables, 
+	Particip
+)
+
+class CanReadTableContent(permissions.BasePermission):
+	"permissions for allowing user read table content"
+
+	edit_methods = ("PUT", "PATCH")
+	
+	def get_url(self):
+		"get table id from url"
+
+		url = self.URL
+		if url.endswith("/"):
+			url=url[:-1]
+		spl = url.split("/")
+		url = spl[spl.index(spl[-1])-1]
+		
+		#makeshift
+		if len(url) == 16:
+			self.table_ID = url
+			return url
+		else: 
+			url = spl[spl.index(spl[-1])]
+			self.table_ID = url
+			return url
+
+
+	def need_password(self):
+		"does table need password?"
+
+		url = self.get_url()
+		try:
+			password = Tables.objects.filter(url=url).first()
+		except:
+			raise NoSuchTable
+		
+		if not password and password.password == "":
+			return False
+		return True
+
+	def already_added(self):
+		"check if user is added to table"
+		is_added = Particip.objects.select_related().filter(user_id__username=self.USERNAME, table_id__url=self.table_ID)
+		is_added = is_added.exists()
+		return is_added
+
+
+	def has_permission(self, request, view):
+		#fist
+		"main function"
+		self.URL = request.path
+		self.table_ID = self.get_url()
+		self.USERNAME = request.session.get("username")
+
+		if request.user.is_authenticated:
+			#id logged
+			if self.already_added():
+				#if added
+				queryset = Particip.objects.select_related().filter(table_id__url=self.table_ID)
+				return queryset
+			else:
+				#not added
+				if not self.need_password():
+					#table doesn;t need password
+					queryset = Particip.objects.select_related().filter(table_id__url=self.table_ID)
+				else:
+					#table needs password and not added
+					raise NotAdded
+
+			return True
+		else:
+			if self.need_password():
+				raise Unauthorized
+			return True
+
+
+
