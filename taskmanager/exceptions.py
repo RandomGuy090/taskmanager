bd093fc761e39ef1b771ba0242f04e5da42a722b
RandomGuy090 <randomguy0090@gmail.com>
2021-09-27 13:10:14 +0200

    exceptions added

---
+++ taskmanager/exceptions.py
@@ -0,0 +1,25 @@
+from rest_framework.exceptions import APIException
+
+class ServiceUnavailable(APIException):
+    status_code = 503
+    default_detail = 'Service temporarily unavailable, try again later.'
+    default_code = 'service_unavailable'
+
+
+class Unauthorized(APIException):
+    status_code = 401
+    default_detail = 'Authorization required'
+    default_code = 'Unauthorized'
+
+
+class NotAdded(APIException):
+    status_code = 401
+    default_detail = 'User not added to the table'
+    default_code = 'Unauthorized'
+
+class NoSuchTable(APIException):
+    status_code = 404
+    default_detail = 'No such table'
+    default_code = 'Not found'
+
+    
