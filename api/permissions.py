90c78a52cf731879aed85cf6ff868136f36b7393
RandomGuy090 <randomguy0090@gmail.com>
2021-10-25 09:13:41 +0200

    permissions

--- api/permissions.py
+++ api/permissions.py
@@ -77,3 +77,31 @@ class CanReadTableContent(permissions.BasePermission):
 			return True
 
 
+
+class CanSendNote(permissions.BasePermission):
+	"permissions for allowing user read table content"
+
+	# edit_methods = ("PUT", "PATCH")
+
+	
+	def already_added(self)-> bool:
+		"check if user is added to table"
+		is_added = Particip.objects.select_related().filter(user_id__username=self.USERNAME, table_id__url=self.table_ID)
+		is_added = is_added.exists()
+		return is_added
+
+
+	def has_permission(self, request, view) -> bool:
+		"main function"
+		self.URL = request.path
+		self.table_ID = get_table_url(request.path)
+		self.USERNAME = request.user.username
+
+
+		if request.user.is_authenticated:
+			return self.already_added()
+
+		else:
+			raise Unauthorized
+
+			#return True
